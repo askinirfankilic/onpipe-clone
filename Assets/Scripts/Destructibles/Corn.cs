a1e8224766672e59a6e39ea640951a6c6f888c99
Irfan Askin Kilic <45240871+kictakimuhabirsincap@users.noreply.github.com>
2021-11-27 17:25:57 +0300

    feat: implement corn particle mechanic

---
+++ Assets/Scripts/Destructibles/Corn.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OnPipe.Destructibles
+{
+    public class Corn : MonoBehaviour, IDestructible
+    {
+        #region Public Fields
+
+        #endregion
+
+        #region Serialized Fields
+
+        [SerializeField] private GameObject particlePrefab;
+        [SerializeField] private GameObject visualPrefab;
+
+        #endregion
+
+        #region Private Fields
+
+        private ParticleSystem particle;
+        private MeshRenderer meshRenderer;
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            particle = particlePrefab.GetComponent<ParticleSystem>();
+            meshRenderer = visualPrefab.GetComponent<MeshRenderer>();
+        }
+        private void OnTriggerEnter( Collider other )
+        {
+            if ( other.CompareTag( Tags.Player ) )
+            {
+                DestroyItself();
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void DestroyItself()
+        {
+            StartCoroutine( WaitForEndOfParticlePlay() );
+        }
+
+        private IEnumerator WaitForEndOfParticlePlay()
+        {
+            if ( particle != null && meshRenderer != null )
+            {
+                meshRenderer.enabled = false;
+                particle.Play();
+                yield return new WaitForSeconds( particle.main.duration );
+                Destroy( gameObject );
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        private void OnEnable()
+        {
+
+        }
+
+        private void OnDisable()
+        {
+
+        }
+
+        private void OnDestroy()
+        {
+            Debug.Log( "Destroyed" );
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Destructibles/IDestructible.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OnPipe.Destructibles
+{
+    public interface IDestructible
+    {
+        public void DestroyItself();
+    }
+}
