a9a831f6ffa204a12e366a38dc0614ac41bfa0f4
Irfan Askin Kilic <45240871+kictakimuhabirsincap@users.noreply.github.com>
2021-12-01 22:00:53 +0300

    build: import gui mono round

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OnPipe
+{
+    public class UIManager : MonoBehaviour
+    {
+        #region Serialized Fields
+
+        [SerializeField] private GameObject home;
+        [SerializeField] private GameObject gameplay;
+        [SerializeField] private GameObject fail;
+        [SerializeField] private GameObject success;
+
+        #endregion
+    }
+}
